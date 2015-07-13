a236e30152c8d0d31a6c35030ae96b7864a0f9a6
Pablo Verdu <panfri787@gmail.com>
2015-07-13 18:34:04 +0200

    Eliminado codigo sobrante de la clase Soilfile y escrita su documentacion

--- src/main/java/pvr3/tfg/domain/Soilcenter.java
+++ src/main/java/pvr3/tfg/domain/Soilcenter.java
@@ -5,18 +5,33 @@ import de.micromata.opengis.kml.v_2_2_0.PolyStyle;
 
 
 /**
- * Created by Pablo on 30/06/2015.
+ * Representation of one line of soilcenter file
  */
 public class Soilcenter {
+    /**
+     * Geounit of the line
+     */
     private String geounit;
 
+    /**
+     * Soil type of the line
+     */
     private int soilType;
 
+    /**
+     * Constructor by params geounit and soilType
+     * @param geounit geounit of the line
+     * @param soilType Soil type of the line
+     */
     public Soilcenter(String geounit, int soilType) {
         this.geounit = geounit;
         this.soilType = soilType;
     }
 
+    /**
+     * Returns the color of every type of soil
+     * @return String with the color
+     */
     private String getColor() {
         switch (this.soilType){
             case 1:
@@ -34,23 +49,15 @@ public class Soilcenter {
         }
     }
 
+    /**
+     * Creates a PolyStyle Kml object to insert into Style object in a Placemark
+     * @return The PolyStyle object with the color of the soil type
+     */
     public PolyStyle getKMLStyle(){
        return KmlFactory.createPolyStyle().withColor(getColor());
     }
 
     public String getGeounit() {
         return geounit;
-    }
-
-    public void setGeounit(String geounit) {
-        this.geounit = geounit;
-    }
-
-    public int getSoiltype() {
-        return soilType;
-    }
-
-    public void setSoiltype(int soiltype) {
-        this.soilType = soiltype;
     }
 }
